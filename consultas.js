2e726d799eaf4758ec46146d8bcb58a3f526a918
CodeWithAdriana <adrianareyesgarcia1@gmail.com>
2024-11-17 13:16:40 -0600

    Dependencias, .env, otros.

--- consultas.js
+++ consultas.js
@@ -5,7 +5,7 @@ const pool = new Pool({
     user: "postgres",
     host: "localhost",
     password: "123456",
-    database: "softjobs",
+    database: "collaresbruno",
     port: 5432,
     allowExitOnIdle: true
 });
@@ -16,7 +16,7 @@ const verificarCredenciales = async (email, password) => {
     const values = [email];
     const { rows, rowCount } = await pool.query(consulta, values);
 
-    if(rowCount<=0)
+    if (rowCount <= 0)
         throw { code: 404, message: "No se encontró ningún usuario con estas credenciales" };
 
     const isMatch = bcrypt.compareSync(password, rows[0].password);
@@ -27,17 +27,17 @@ const verificarCredenciales = async (email, password) => {
 const getUserByEmail = async (email) => {
     const consulta = "SELECT * FROM usuarios WHERE email = $1";
     const values = [email];
-    const {rows} = await pool.query(consulta, values);
+    const { rows } = await pool.query(consulta, values);
     return rows[0];
 }
 
-const agregarUser = async ({email,password,rol,lenguage}) => {
+const agregarUser = async ({ email, password, rol, lenguage }) => {
     const consulta = "INSERT INTO usuarios VALUES (DEFAULT, $1,$2,$3,$4) RETURNING *";
     const hash = bcrypt.hashSync(password, 10);
-    console.log('hash',hash);
+    console.log('hash', hash);
     const values = [email, hash, rol, lenguage];
     const res = await pool.query(consulta, values);
     return res.rows[0];
 }
 
-module.exports = {verificarCredenciales,getUserByEmail,agregarUser}
+module.exports = { verificarCredenciales, getUserByEmail, agregarUser }
--- index.js
+++ index.js
@@ -1,22 +1,24 @@
+require('dotenv').config();
 const { verificarCredenciales, getUserByEmail, agregarUser } = require('./consultas');
 const { verificarCredencialesMiddleware, validarTokenMiddleware } = require('./middlewares');
-const { JWT_SECRET_KEY } = require('./config'); 
+const { JWT_SECRET_KEY } = require('./config');
 const cors = require("cors");
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const morgan = require('morgan');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT_SERVER || 3000;
+const JWT_SECRET_KEY = process.env.SECRET_JWT_KEY;
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
 
-app.get("/", async (req,res) => {
+app.get("/", async (req, res) => {
   try {
-    res.json("Bienvenido a la api");
+    res.json("Bienvenido a la API  de Collares Bruno");
   } catch (error) {
     res.status(500).json("Un error ha ocurrido: " + error);
   }
@@ -24,43 +26,43 @@ app.get("/", async (req,res) => {
 
 
 app.post("/login", verificarCredencialesMiddleware, async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        await verificarCredenciales(email, password);
-        const token = jwt.sign({ email }, JWT_SECRET_KEY);
-        res.send({token});
-    } catch (error) {
-        console.log(error);
-        res.status(error.code || 500).send(error);
-    }
+  try {
+    const { email, password } = req.body;
+    await verificarCredenciales(email, password);
+    const token = jwt.sign({ email }, JWT_SECRET_KEY);
+    res.send({ token });
+  } catch (error) {
+    console.log(error);
+    res.status(error.code || 500).send(error);
+  }
 });
 
 
 app.post("/usuarios", verificarCredencialesMiddleware, async (req, res) => {
-    try {
-        const { email, password, rol, lenguage } = req.body;
-        console.log(email, password, rol, lenguage);
+  try {
+    const { email, password, rol, lenguaje } = req.body;
+    console.log(email, password, rol, lenguaje);
 
-        const usuarioAgregado = await agregarUser({email, password, rol, lenguage});
-        console.log(usuarioAgregado);
+    const usuarioAgregado = await agregarUser({ email, password, rol, lenguage });
+    console.log(usuarioAgregado);
 
-        res.json({mensaje:"ok"});
-    } catch (error) {
-        res.status(500).send(error);
-    }
+    res.json({ mensaje: "Usuario agregado con éxito" });
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 
 app.get("/usuarios", validarTokenMiddleware, async (req, res) => {
-    try {
-        
-      const { email } = req.user;
-      const user = await getUserByEmail(email);
-      res.json([user]);
-      
-    } catch (error) {
-        res.status(500).send(error);
-    }
+  try {
+
+    const { email } = req.user;
+    const user = await getUserByEmail(email);
+    res.json(user);
+
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 
